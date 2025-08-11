4bf2ed59f06f949366b403b7d8d15ad8cca89cf2
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-11 12:17:21 +0000

    favicon

--- src/lib/translations/bn/site.ts
+++ src/lib/translations/bn/site.ts
@@ -1,8 +1,9 @@
+
 export const site = {
   "title": "Mojib Rsm",
   "url": "https://www.mojib.oftern.com",
   "logo": "/uploads/1752060795135-aulbtu.jpg",
   "publicLogo": "/uploads/1752063003787-Mojib.gif",
   "adminAvatar": "/uploads/1754912062629-Mojib_Rsm.jpg",
-  "favicon": "/favicon.ico"
+  "favicon": "https://i.postimg.cc/8P04g40T/man.png"
 };
