a450131ede1e6d18adef8c28f4549eb8798c3d9f
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-11 12:26:43 +0000

    og image eta daw..

--- src/lib/translations/bn/site.ts
+++ src/lib/translations/bn/site.ts
@@ -4,6 +4,6 @@ export const site = {
   "url": "https://www.mojib.oftern.com",
   "logo": "/uploads/1752060795135-aulbtu.jpg",
   "publicLogo": "/uploads/1752063003787-Mojib.gif",
-  "adminAvatar": "/uploads/1754912062629-Mojib_Rsm.jpg",
+  "adminAvatar": "https://i.postimg.cc/8P04g40T/man.png",
   "favicon": "https://i.postimg.cc/8P04g40T/man.png"
 };
